171a58181fc3b568a9cd0dc0b17123b5aa772f7f
Thuyho1502 <p2thanhthuy1502@gmail.com>
2025-03-17 21:42:05 +0100

    character card show information

---
+++ character.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", async () =>{
+    const characterList = document.getElementById("character_list");
+
+    let allCharacters =[];
+    let nextURL = "https://swapi.dev/api/people";
+
+    try{
+        while(nextURL){
+            const response = await fetch(nextURL);
+            const data = await response.json();
+            allCharacters = allCharacters.concat(data.results);
+            nextURL = data.next;    // get next page URL
+        }
+
+        for(const character of allCharacters) {
+            const characterDiv = document.createElement("div");
+            characterDiv.classList.add("characters_card");
+
+            // Fetch Films
+            const filmTitle = await Promise.all(
+                character.films.map (async(filmUrl)=>{
+                    const filmResponse = await fetch(filmUrl);
+                    const filmData = await filmResponse.json();
+                    return filmData.title;
+                })
+            );
+
+            // Fetch Species
+            let speciesName = "Unknown";
+            if(character.species.length >0) {
+                const speciesResponse = await fetch(character.species[0]);
+                const speciesData = await speciesResponse.json();
+                speciesName = speciesData.name;
+
+            }
+
+            //Populate Character Card
+            characterDiv.innerHTML = `
+            <h2> ${character.name} </h2>
+            <p> <strong> Birth Year : <strong> ${character.birth_year}</p>
+            <p> <strong> Films : <strong> ${filmTitle.join(",")}</p>
+            <p> <strong> Species : <strong> ${speciesName}</p>
+            `
+            characterList.appendChild(characterDiv);
+        }
+
+    } catch (error){
+        console.error("Error fetching Star Wars character:", error);
+    }
+
+});
